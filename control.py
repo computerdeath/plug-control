a3ae3c818d79ceb9ff33c022396f79f6c652c136
Joshua Serrano <josh@Joshuas-MacBook-Pro.local>
2021-01-02 21:10:20 -0800

    change to token file

--- control.py
+++ control.py
@@ -2,9 +2,15 @@ import aiohttp
 import pysmartthings
 import asyncio
 
-token = ''
 device_name = ''
-async def control():
+
+def getToken():
+    token_file = open('token.key','r')
+    token = token_file.read().strip()
+    token_file.close()
+    return token
+
+async def control(token):
     async with aiohttp.ClientSession() as session:
         api = pysmartthings.SmartThings(session,token)
         devices = await api.devices()
@@ -17,4 +23,5 @@ async def control():
                 if state == 'off':
                     await device.command("main","switch","on")
 if __name__ == "__main__":
-    asyncio.run(control())
+    token = getToken()
+    asyncio.run(control(token))
